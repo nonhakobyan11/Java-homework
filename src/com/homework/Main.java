bb0c7447e4a8ee95466bf3ac1eb19ee229d901aa
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-13 00:45:43 +0400

    Problem 8

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void declareArrayAndPrintElements(){
         String[] str = {"yellow", "green", "blue", "red", "purple"};
 	    for(int i = 0; i < str.length; i++){
-	        System.out.print(str[i] + " ");
+	        System.out.print(str[i] + ", ");
         }
         System.out.println();
     }
@@ -15,7 +15,7 @@ public class Main {
     public static void declareIntArrayWithZeroes(){
         int[] arr = new int[15] ;
         for (int i = 0; i < 15; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + ", ");
         }
         System.out.println();
     }
@@ -29,7 +29,7 @@ public class Main {
         }
         // tpel ayn
         for (int j = 1; j <= 1000; j++) {
-            System.out.print(arr[j] + " ");
+            System.out.print(arr[j] + ", ");
         }
         System.out.println();
     }
@@ -44,7 +44,7 @@ public class Main {
 	        bArr[j++] = (byte)i;
         }
 	    for (int i = 0; i < 30; i++){
-            System.out.print(bArr[i] + " ");
+            System.out.print(bArr[i] + ", ");
         }
         System.out.println();
     }
@@ -57,7 +57,7 @@ public class Main {
             arr[j++] = i;
         }
         for (int i = 0; i < 150; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + ", ");
         }
         System.out.println();
     }
@@ -67,12 +67,33 @@ public class Main {
         int[] arr = {1, 5, 4, -47, 64, 25, 78, 625};
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 5 == 0) {
-                System.out.print(arr[i] + " ");
+                System.out.print(arr[i] + ", ");
             }
         }
         System.out.println();
     }
 
+    // Problem 7
+    public static void printElGreaterThenInDoubleArray (){
+        double[] arr = {18.37, 68.7, 71.4, 24.68, 44.8, 271.1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 24.56) {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.println();
+    }
+
+    // Problem 8
+    public static void printElGreatOrSmallInDoubleArray(){
+        float[] arr = {48.49F, 24.78F, 65.74F, -21.174F, -45.47F};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 35.56F || arr[i] < -34.655F) {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         System.out.print("Problem 1 output: "); declareArrayAndPrintElements();
@@ -81,5 +102,7 @@ public class Main {
         System.out.print("Problem 4 output: "); printEvenElOfByteArray();
         System.out.print("Problem 5 output: "); printOddElOfShortArray();
         System.out.print("Problem 6 output: "); printElDividedByFiveInIntArray();
+        System.out.print("Problem 7 output: "); printElGreaterThenInDoubleArray ();
+        System.out.print("Problem 8 output: "); printElGreatOrSmallInDoubleArray();
     }
 }
