0cefd8fbd9f52485038d462d76415677362f5adc
Nonna Hakobyan <nonhakobyan11@gmail.com>
2019-12-13 00:15:29 +0400

    Problem 3

--- src/com/homework/Main.java
+++ src/com/homework/Main.java
@@ -20,8 +20,23 @@ public class Main {
         System.out.println();
     }
 
+    //Problem 3
+    public static void declareIntArrayWithThousandElements() {
+        int[] arr = new int[1001];
+        //lcnel 1-1000 tverov
+        for (int i = 1; i <= 1000; i++) {
+            arr[i] = i;
+        }
+        // tpel ayn
+        for (int j = 1; j <= 1000; j++) {
+            System.out.print(arr[j] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         System.out.print("Problem 1 output: "); declareArrayAndPrintElements();
         System.out.print("Problem 2 output: "); declareIntArrayWithZeroes();
+        System.out.print("Problem 3 output: "); declareIntArrayWithThousandElements();
     }
 }
